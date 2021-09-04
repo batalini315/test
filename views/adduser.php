a40e11596268a9fc602ea1a956d8daf74e831b71
Batalini <315batalini@gmail.com>
2021-09-04 12:43:36 +0300

    router4

---
+++ views/adduser.php
@@ -0,0 +1,23 @@
+<div class="row align-items-start">
+    <div class="col"></div>
+    <div class="col">
+<form action="adduser" method="get">
+    <label for="email">Email</label>
+  <p><input type="email" name="email"></p>
+  <label for="name">Имя</label>
+  <p><input type="text" name="name"></p>
+  <label for="addres">Адрес</label>
+  <p><input type="text" name="addres"></p>
+  <label for="phone">Телефон</label>
+  <p><input type="text" name="phone"></p>
+  <label for="comment">Коментарий</label>
+  <p><input type="text" name="comment"></p>
+  <label for="otdel">Отдел</label>
+  <p><input type="text" name="otdel"></p>
+  <p><input type="submit" value="Отправить"></p>
+ </form>
+</div>
+    <div class="col"></div>
+</div>
+
+<!--  -->
